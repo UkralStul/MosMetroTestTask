5145cc37a5a889bb763458de5f7b88bb55a540c7
UkralStul <90962034+UkralStul@users.noreply.github.com>
2025-06-05 19:55:51 +0300

    Update config.py

--- MosMetroTestTaskServer/core/config.py
+++ MosMetroTestTaskServer/core/config.py
@@ -3,7 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
-    db_url: str = "postgresql+asyncpg://postgres:7243@localhost:5432/MosMetroTestTask"
+    db_url: str = "postgresql+asyncpg://postgres:7243@db:5432/moscowmapdb"
     echo: bool = False
 
 settings = Settings()
